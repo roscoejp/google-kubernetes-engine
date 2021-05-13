06d523ac8e4cb0f6f3572a323eeda896ff41c6c4
Roscoe Pyell <roscoejp@gmail.com>
2021-05-13 11:14:57 -0400

    Update transcode.sh

--- ffmpeg/transcode.sh
+++ ffmpeg/transcode.sh
@@ -7,8 +7,8 @@ echo "Starting Job: ${JOB_NAME}"
 # Check ffmpeg version
 ffmpeg -version
 
-# Remote filename
-VIDEO_SOURCE="https://github.com/Matroska-Org/matroska-test-files/blob/master/test_files/test1.mkv?raw=true"
+# Remote filename (this should be set in job env vars)
+# VIDEO_SOURCE="https://github.com/Matroska-Org/matroska-test-files/blob/master/test_files/test1.mkv?raw=true"
 
 # Download file
 START_TIME=$(date +%s)
